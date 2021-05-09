734e7016ba60bb9e9850f92522a69ee9e2b122d2
kbc18a11 <wada121400@gmail.com>
2021-05-09 22:16:21 +0900

    次58

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { IteratorController } from './iterator/iterator.controller';
 import { BookShelfService } from './iterator/book-shelf.service';
 import { BookShelfIteratorService } from './iterator/book-shelf-iterator.service';
+import { SingletonController } from './singleton/singleton.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, IteratorController],
+  controllers: [AppController, IteratorController, SingletonController],
   providers: [AppService, BookShelfService, BookShelfIteratorService],
 })
 export class AppModule { }
